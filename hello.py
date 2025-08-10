cb979a1711f770c89d3815136b9a6d7816d0b0c9
Sayanjit Mukherjee <silverfangthegem@gmail.com>
2025-08-10 16:02:27 +0530

    added static files.

--- hello.py
+++ hello.py
@@ -1,13 +1,18 @@
-from flask import Flask
+from flask import Flask,url_for
+
 from markupsafe import escape
 
-app = Flask(__name__)
+app = Flask(__name__) #object of flask class
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hey niggas</h1>"
+    return 'Hey niggas this is Home page'
 
 
 @app.route("/<name>&&<int:id>")
 def get_username(name : str, id : int):
     return f'Username is {escape(name)} and id is {id}' #doing it cozz its a string and preventing url injection attacks
+
+# with app.test_request_context():
+#     print(url_for(get_username))
+#     print(url_for(hello_world))
